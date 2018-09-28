9c049330c44d12f0ebf793d17411552b6a5be068
witty-collection-3981 <yisroel96@gmail.com>
2018-09-28 18:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays(string) {
+ if string === string.happyHolidays()
+ return "Happy holidays!"
+ }
+
+
+
+
+
+//  expect(happyHolidays()).toEqual('Happy holidays!')
